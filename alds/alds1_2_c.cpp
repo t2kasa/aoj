9303c3aa88ffd48a42d077b228d2e1ab96500056
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-08-15 19:08:50 +0900

    solve alds1_2_c

--- alds/alds1_2_c.cpp
+++ alds/alds1_2_c.cpp
@@ -1,12 +1,21 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_2_C
+// Sort I - Stable Sort
+
+#define SUBMIT
+
+#include <utility>
 #include <iostream>
-#include <array>
+#include <iomanip>
+#include <vector>
+#include <set>
 
-class card
-{
+using namespace std;
+
+class card {
 public:
-    card() : card('\0', 0) { }
+    card() : card('\0', 0) {}
 
-    card(char suit, int value) : suit_(suit), value_(value) { }
+    card(char suit, int value) : suit_(suit), value_(value) {}
 
     char suit() { return suit_; }
 
@@ -21,71 +30,56 @@ private:
     int value_;
 };
 
-void print_card(card c[], int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        if (0 < i)
-        {
-            std::cout << " ";
-        }
+void swap(card& a, card& b) {
+    auto v_temp = a.value();
+    a.value(b.value());
+    b.value(v_temp);
 
-        std::cout << c[i].suit() << c[i].value();
-    }
-
-    std::cout << std::endl;
+    auto s_temp = a.suit();
+    a.suit(b.suit());
+    b.suit(s_temp);
 }
 
-void bubble_sort(card a[], int n)
-{
-    for (auto i = 0; i < n; ++i)
-    {
-        for (auto j = n - 1; i <= j; ++j)
-        {
-            if (a[j].value() < a[j - 1].value())
-            {
-                card temp = a[j];
-                a[j] = a[j - 1];
-                a[j - 1] = temp;
-            }
+void print_card(card c[], int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << c[i].suit() << c[i].value();
+
+        if (i != n - 1) cout << " ";
+        else cout << endl;
+    }
+}
+
+void bubble_sort(card c[], int n) {
+    for (auto i = 0; i < n; ++i) {
+        for (auto j = n - 1; i < j; --j) {
+            if (c[j].value() < c[j - 1].value()) swap(c[j], c[j - 1]);
         }
     }
 }
 
-void selection_sort(card a[], int n)
-{
-    for (auto i = 0; i < n; ++i)
-    {
+void selection_sort(card c[], int n) {
+    for (auto i = 0; i < n; ++i) {
         auto min_index = i;
-        for (auto j = i; j < n; ++j)
-        {
-            if (a[j].value() < a[min_index].value())
-            {
-                min_index = j;
-            }
+        for (auto j = i; j < n; ++j) {
+            if (c[j].value() < c[min_index].value()) min_index = j;
         }
 
-        card temp = a[i];
-        a[i] = a[min_index];
-        a[min_index] = temp;
+        swap(c[i], c[min_index]);
     }
 }
 
 
-int main()
-{
-    std::size_t n;
-    std::cin >> n;
+int main() {
+    int n;
+    cin >> n;
 
     card c_bubble[n];
     card c_selection[n];
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         char suit;
         int value;
 
-        std::cin >> suit;
-        std::cin >> value;
+        cin >> suit >> value;
 
         c_bubble[i].suit(suit);
         c_bubble[i].value(value);
@@ -94,11 +88,22 @@ int main()
         c_selection[i].value(value);
     }
 
+    // bubble sort is always stable.
     bubble_sort(c_bubble, n);
     print_card(c_bubble, n);
-//    selection_sort(c_selection, n);
-//
-//    print_card(c_bubble, n);
+    cout << "Stable" << endl;
 
+    selection_sort(c_selection, n);
+    print_card(c_selection, n);
+
+    auto selection_stable = true;
+    for (int i = 0; i < n; ++i) {
+        if (c_bubble[i].suit() != c_selection[i].suit() || c_bubble[i].value() != c_selection[i].value()) {
+            selection_stable = false;
+        }
+    }
+
+    if (selection_stable) cout << "Stable" << endl;
+    else cout << "Not stable" << endl;
     return 0;
 }
